d5c70047b44e8f76c897b866608299d7c1b41709
luisyge35 <luisys97@gmail.com>
2020-07-04 17:42:55 +0200

    Add shows

--- routes/posts.js
+++ routes/posts.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { errorHandler } = require('../middleware');
+const { getPosts, 
+        newPost,
+        createPost,
+        showPost } = require('../controllers/posts');
+const { create } = require('../models/user');
 
 /* GET posts index /posts  */
-router.get('/', (req, res, next) =>  {
-    res.send('INDEX /posts');
-  });
+router.get('/', errorHandler(getPosts));
 
 /* GET --> new --> /posts/new */
-router.get('/new', (req, res, next) =>  {
-    res.send('NEW /posts/new');
-  });
+router.get('/new', newPost);
 
 /* POST --> create --> /posts */
-router.post('/', (req, res, next) =>  {
-    res.send('CREATE /posts');
-  });
+router.post('/', errorHandler(createPost));
 
 /* GET --> show --> /posts/:id */
-router.get('/:id', (req, res, next) =>  {
-    res.send('SHOW /posts/:id');
-  });
+router.get('/:id', errorHandler(showPost));
 
 /* GET --> edit --> /posts/:id/edit */
 router.get('/:id/edit', (req, res, next) =>  {
