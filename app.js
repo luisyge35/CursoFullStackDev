fea9e7e50fc69c8c4fa6b0b15e28484aae6010a0
luisyge35 <luisys97@gmail.com>
2020-07-08 18:25:01 +0200

    Add flash messages

--- app.js
+++ app.js
@@ -55,9 +55,17 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-//ttile middleware
+//set local variables middleware
 app.use(function(req, res, next) {
+  // Set default page message
   res.locals.title = "Surf shop";
+  // Set success flash message
+  res.locals.success = req.session.success || '';
+  delete req.session.success;
+  // Set error flash message
+  res.locals.error = req.session.error || '';
+  delete req.session.error;
+  // Continue with next message in middleware chain
   next()
 })
 
@@ -74,12 +82,15 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // // render the error page
+  // res.status(err.status || 500);
+  // res.render('error');
+  console.log(err);
+  req.session.error = err.message;
+  res.redirect('back');
 });
 
 
