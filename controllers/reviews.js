ae56113e564db8dd743c3423c9f132113373d7c1
luisyge35 <luisys97@gmail.com>
2020-07-09 16:39:05 +0200

    Add author to posts reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,27 +1,29 @@
-const Post = require ('../models/post');
+const Post = require('../models/post');
 const Review = require('../models/review');
 
+
 module.exports = {
-    async reviewCreate(req, res, next) {
-        // find the post by its id
-        let post = await Post.findById(req.params.id);
-        // create the review
-        req.body.review.author = req.user._id;
-        let review = await Review.create(req.body.review);
-        // assign the review to a post 
-        post.reviews.push(review);
-        // save the post
-        post.save();
-        // redirect to the post 
-        req.session.success = 'Review created succesfully';
-        res.redirect(`/posts/${post.id}`);
-    },
+	// Reviews Create
+	async reviewCreate(req, res, next) {
+		// find the post by its id
+		let post = await Post.findById(req.params.id);
+		// create the review
+		req.body.review.author = req.user._id;
+		let review = await Review.create(req.body.review);
+		// assign review to post
+		post.reviews.push(review);
+		// save the post
+		post.save();
+		// redirect to the post
+		req.session.success = 'Review created successfully!';
+		res.redirect(`/posts/${post.id}`);
+	},
+	// Reviews Update
+	async reviewUpdate(req, res, next) {
+	
+	},
+	// Reviews Destroy
+	async reviewDestroy(req, res, next) {
 
-    async reviewUpdate(req, res, next) {
-
-    },
-
-    async reviewDelete(req, res, next) {
-        
-    }
-} 
+	}
+}
